140466206bf4affdd1607d63dafbf93a4f2cebc7
Neous1 <Neous1@users.noreply.github.com>
2017-08-12 09:15:40 -0400

    link style.css iterate thre results make description clickable

--- script.js
+++ script.js
@@ -9,16 +9,22 @@ $(document).ready(function () {
         var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + query + "&format=json&callback=?"
 
         $.getJSON(url, function (data) {
-            // console.log(data)
-
+            console.log(data)
+//iterate thru search result
             for (var i = 0; i < data[1].length; i++) {
-                console.log(data[1][i])
+                // console.log(data[1][i])
                 // console.log(data[3][i])
+// $("#results").html(" ");
+                $("#results").append("<li data-link= '" + data[3][i]+" '>" + data[1][i] + "<br>" + data[2][i]+"</li>")
 
-                $("#results").append("<li>" + data[1][i] + "<br>" + data[2][i] + "<br>" + data[3][i] + "</li>")
-
-                //data[1][i]
             }
         })
     })
+    $("#results").on("click", "li",function(){
+        var link = $(this).attr("data-link");
+        window.open(link);
+    })
 })
+
+
+Document.write(getelementbyID)
